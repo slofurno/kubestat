48481de62dac2078e1c4628ef5e6887078936983
Steve LoFurno <slofurno@gmail.com>
2017-10-11 14:31:52 -0400

    push stats

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -19,13 +23,26 @@ func init() {
 
 func main() {
 
+	endpoint := os.Getenv("DRAIN_ENDPOINT")
+
+	client := http.Client{}
+
 	s := New(rootDir)
 
 	for {
 		s.Refresh()
 
-		for id, pod := range s.pods {
-			fmt.Printf("[%s] %#v\n", id, pod)
+		pods := []*PodStat{}
+		for _, pod := range s.pods {
+			pods = append(pods, pod)
+		}
+
+		b, _ := json.Marshal(pods)
+		req, _ := http.NewRequest("POST", endpoint, bytes.NewReader(b))
+		_, err := client.Do(req)
+
+		if err != nil {
+			fmt.Println(err)
 		}
 
 		time.Sleep(5 * time.Second)
@@ -46,18 +63,21 @@ func New(root string) *Stats {
 }
 
 type PodStat struct {
+	Id string
 	//nanoseconds
-	cpuacct_usage    int64
-	cpuacct_usage_d  int64
-	nr_throttled     int64
-	throttled_time   int64
-	throttled_time_d int64
-	total_rss        int64
-	total_cache      int64
+	Cpuacct_usage    int64
+	Cpuacct_usage_d  int64
+	Nr_throttled     int64
+	Throttled_time   int64
+	Throttled_time_d int64
+	Total_rss        int64
+	Total_cache      int64
 
 	//microseconds
-	cpu_cfs_quota_us  int64
-	cpu_cfs_period_us int64
+	Cpu_cfs_quota_us  int64
+	Cpu_cfs_period_us int64
+
+	Time time.Time
 }
 
 func matchName(n string) bool {
@@ -75,7 +95,7 @@ func (s *Stats) Refresh() {
 				var pod *PodStat
 				var ok bool
 				if pod, ok = s.pods[x.Name()]; !ok {
-					pod = &PodStat{}
+					pod = &PodStat{Id: x.Name()}
 					s.pods[x.Name()] = pod
 				}
 
@@ -84,10 +104,10 @@ func (s *Stats) Refresh() {
 
 				cu := strings.Split(string(n), "\n")[0]
 				cpuacct_usage, _ := strconv.ParseInt(cu, 10, 64)
-				if pod.cpuacct_usage > 0 {
-					pod.cpuacct_usage_d = cpuacct_usage - pod.cpuacct_usage
+				if pod.Cpuacct_usage > 0 {
+					pod.Cpuacct_usage_d = cpuacct_usage - pod.Cpuacct_usage
 				}
-				pod.cpuacct_usage = cpuacct_usage
+				pod.Cpuacct_usage = cpuacct_usage
 
 				b := base + "/" + x.Name() + "/" + "cpu.stat"
 				n, _ = ioutil.ReadFile(b)
@@ -97,24 +117,24 @@ func (s *Stats) Refresh() {
 				nr_throttled := strings.Split(lines[1], " ")[1]
 				th := strings.Split(lines[2], " ")[1]
 
-				pod.nr_throttled, _ = strconv.ParseInt(nr_throttled, 10, 64)
+				pod.Nr_throttled, _ = strconv.ParseInt(nr_throttled, 10, 64)
 				throttled_time, _ := strconv.ParseInt(th, 10, 64)
-				if pod.throttled_time > 0 {
-					pod.throttled_time_d = throttled_time - pod.throttled_time
+				if pod.Throttled_time > 0 {
+					pod.Throttled_time_d = throttled_time - pod.Throttled_time
 				}
-				pod.throttled_time = throttled_time
+				pod.Throttled_time = throttled_time
 
 				b = base + "/" + x.Name() + "/" + "cpu.cfs_period_us"
 				n, _ = ioutil.ReadFile(b)
 
 				period := strings.Split(string(n), "\n")[0]
-				pod.cpu_cfs_period_us, _ = strconv.ParseInt(period, 10, 64)
+				pod.Cpu_cfs_period_us, _ = strconv.ParseInt(period, 10, 64)
 
 				b = base + "/" + x.Name() + "/" + "cpu.cfs_quota_us"
 				n, _ = ioutil.ReadFile(b)
 
 				quota := strings.Split(string(n), "\n")[0]
-				pod.cpu_cfs_quota_us, _ = strconv.ParseInt(quota, 10, 64)
+				pod.Cpu_cfs_quota_us, _ = strconv.ParseInt(quota, 10, 64)
 			}
 		}
 
@@ -140,10 +160,10 @@ func (s *Stats) Refresh() {
 					next := strings.Split(lines[i], " ")
 					switch next[0] {
 					case "total_rss":
-						pod.total_rss, _ = strconv.ParseInt(next[1], 10, 64)
+						pod.Total_rss, _ = strconv.ParseInt(next[1], 10, 64)
 
 					case "total_cache":
-						pod.total_cache, _ = strconv.ParseInt(next[1], 10, 64)
+						pod.Total_cache, _ = strconv.ParseInt(next[1], 10, 64)
 					}
 
 				}
